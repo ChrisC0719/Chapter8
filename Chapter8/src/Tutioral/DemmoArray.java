bab25e58fad38222143c9932fd7cba130be296da
ChrisC0719 <31348437+ChrisC0719@users.noreply.github.com>
2017-10-03 09:24:34 -0500

    updates

---
+++ Chapter8/src/Tutioral/DemmoArray.java
@@ -0,0 +1,17 @@
+package Tutioral;
+
+public class DemmoArray {
+
+	public static void main(String[] args) 
+	{
+		 double[] salaries = { 6.25, 6.55, 10.25, 16.85};
+		  System.out.println("Salaries one by one are:");
+		
+		  
+		  for(int x = 0; x < salaries.length; ++x)
+	      System.out.println(salaries[x]);
+	     
+
+	}
+
+}
